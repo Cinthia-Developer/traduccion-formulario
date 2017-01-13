055d97d482a82200df0d928f22fd88c3c6030d54
Cinthia <cinthiamashiel474@gmail.com>
2017-01-13 16:27:46 -0500

    traducido

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// para modificar el texto de un <div>
+document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesión";
+// para modificar el placeholder de un input
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+// para modificar un span (checkbox)
+document.querySelector("span").innerHTML="Recordar datos";
+// para modicar el contenido de un <button>
+document.querySelector("button").innerHTML="Iniciar Sesión";
